17bede928cb4612681cb98d52489171b3278e713
Ariel Orozco <aorozco@fh.org>
2016-11-25 21:58:56 -0600

    Finishing the course

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,105 @@
 var testingAngularApp = angular.module('testingAngularApp', []);
 
-testingAngularApp.controller('testingAngularCtrl', function($rootScope, $scope) {
+
+// Aoo controller
+testingAngularApp.controller('testingAngularCtrl', function($rootScope, $scope, $http, $log, $timeout, helperService) {
+
     $scope.title = "Testing AngularJS Applications";
+
+    $scope.destinations = [];
+
+    $scope.apiKey = "fba96dd8b75cb80c55412f58297e7516";
+
+    $scope.newDestination = {
+        city: undefined,
+        country: undefined
+    };
+
+    $scope.addDestination = function(){
+        $scope.destinations.push({
+            city: $scope.newDestination.city,
+            country: $scope.newDestination.country
+        });
+    };
+
+    $scope.removeDestination = function(index){
+        $scope.destinations.splice(index, 1);
+    };
+
+    $scope.messageWatcher = $rootScope.$watch('message', function(){
+        if($rootScope.message){
+            $timeout(function(){
+                $rootScope.message = null;
+            }, 3000);
+        }
+    });
+
+});
+
+
+// Custom filter
+testingAngularApp.filter('warmestDestinations', function() {
+    return function(destinations, minimumTemp){
+        var warmDestinations = [];
+
+        angular.forEach(destinations, function(destination){
+            if(destination.weather && destination.weather.temp && destination.weather.temp >= minimumTemp){
+                warmDestinations.push(destination);
+            }
+        });
+
+        return warmDestinations;
+    };
+});
+
+
+// Custom service
+testingAngularApp.service('helperService', function() {
+    this.convertKelvinToCelsius = function(temp){
+        return Math.round(temp - 273);
+    };
+
+    return this;
+});
+
+
+// Custom directive
+testingAngularApp.directive('destinationDirective', function() {
+    return {
+        scope: {
+            destination: '=',
+            apiKey: '=',
+            onRemove: '&'
+        },
+        template:
+                '<span>{{destination.city}}, {{destination.country}}</span>' +
+                '<span ng-if="destination.weather">' +
+                '    - {{destination.weather.main}}, {{destination.weather.temp}}' +
+                '</span>' +
+                '<button ng-click="onRemove()">Remove</button>' +
+                '<button ng-click="getWeather(destination)">Update Weather</button>',
+        controller: function($http, $rootScope, $scope, helperService){
+
+            $scope.getWeather = function(destination){
+                $http.get("http://api.openweathermap.org/data/2.5/weather?q="+destination.city+"&appid="+$scope.apiKey).then(
+                    function successCallback(response){
+                        if(response.data.weather){
+                            destination.weather = {};
+                            destination.weather.main = response.data.weather[0].main;
+                            destination.weather.temp = helperService.convertKelvinToCelsius(response.data.main.temp);
+                        } else {
+                            $rootScope.message = "City not found";
+                        }
+                    },
+                    function errorCallback(error){
+                        $rootScope.message = "Server Error";
+                        $rootScope.$broadcast('messageUpdated', { type: 'error', message: 'Server error' });
+                    }
+                );
+            };
+
+        }
+
+    };
+
 });
--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -1,28 +1,309 @@
-describe('Testing AngularJS Test Suite', function() {
+describe('Testing AngularJS Test Suite => ', function() {
 
     // Inject the app module that we are testing
     beforeEach(module('testingAngularApp'));
 
-    describe('Testing AngularJS Controller', function() {
+    describe('Testing AngularJS Controller => ', function() {
 
-        var scope, ctrl;
+        // Global variables to the controller
+        var scope, ctrl, httpBackend, timeout, rootScope;
 
-        // Inject the controller and scope and assign them to global variables
-        beforeEach(inject(function($controller, $rootScope){
+        // Inject the controller, scope and any other services and assign them to global variables
+        beforeEach(inject(function($controller, $rootScope, $httpBackend, $timeout){
+            rootScope = $rootScope;
             scope = $rootScope.$new();
             ctrl = $controller('testingAngularCtrl', {$scope:scope});
+            httpBackend = $httpBackend;
+            timeout = $timeout;
         }));
 
+        // Cleanup code
         afterEach(function(){
-            // Cleanup code
-
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
         });
 
         // This is a test
         it('should initialize the title in the scope', function() {
-
             expect(scope.title).toBeDefined();
             expect(scope.title).toBe('Testing AngularJS Applications');
+        });
+
+        it('should add 2 destinations to the destinations list', function() {
+            expect(scope.destinations).toBeDefined();
+            expect(scope.destinations.length).toBe(0);
+
+            scope.newDestination = {
+                city: "Cartago",
+                country: "Costa Rica"
+            };
+
+            scope.addDestination();
+
+            expect(scope.destinations.length).toBe(1);
+            expect(scope.destinations[0].city).toBe("Cartago");
+            expect(scope.destinations[0].country).toBe("Costa Rica");
+
+            scope.newDestination.city = "Phoenix";
+            scope.newDestination.country = "USA";
+
+            scope.addDestination();
+
+            expect(scope.destinations.length).toBe(2);
+            expect(scope.destinations[1].city).toBe("Phoenix");
+            expect(scope.destinations[1].country).toBe("USA");
+
+            expect(scope.destinations[0].city).toBe("Cartago");
+            expect(scope.destinations[0].country).toBe("Costa Rica");
+        });
+
+        it('should remove a destination from the destinations list', function() {
+            scope.destinations = [
+                {
+                    city: "Paris",
+                    country: "France"
+                },
+                {
+                    city: "Warsaw",
+                    country: "Poland"
+                }
+            ];
+
+            expect(scope.destinations.length).toBe(2);
+
+            scope.removeDestination(0);
+
+            expect(scope.destinations.length).toBe(1);
+            expect(scope.destinations[0].city).toBe("Warsaw");
+            expect(scope.destinations[0].country).toBe("Poland");
+        });
+
+        it('should remove error message after a fixed period of time', function(){
+            rootScope.message = "Error";
+            expect(rootScope.message).toBe("Error");
+
+            // Needs this to complete the digest cycle
+            rootScope.$apply();
+
+            // Also this to end any pending process
+            timeout.flush();
+
+            expect(rootScope.message).toBeNull();
+        });
+
+    });
+
+    // No need to test this service since it's mocked on the directive
+    /*describe('Testing AngularJS Service => ', function(){
+
+        it('should return celsius temperature from kelvin', inject(function(helperService){
+            var temp = 290;
+
+            var tempInCelsius = helperService.convertKelvinToCelsius(temp);
+
+            expect(tempInCelsius).toBe(17);
+        }));
+
+    });*/
+
+    describe('Testing AngularJS Filter => ', function(){
+
+        it('should return only warm destinations', inject(function($filter){
+            var warmest = $filter('warmestDestinations');
+
+            var destinations = [
+                {
+                    city: "Beijing",
+                    country: "China",
+                    weather: {
+                        temp: 21
+                    }
+                },
+                {
+                    city: "Moscow",
+                    country: "Russia"          
+                },
+                {
+                    city: "Mexico City",
+                    country: "Mexico",
+                    weather: {
+                        temp: 12
+                    }
+                },
+                {
+                    city: "Lima",
+                    country: "Peru",
+                    weather: {
+                        temp: 15
+                    }
+                }
+            ];
+
+            expect(destinations.length).toBe(4);
+
+            var warmDestinations = warmest(destinations, 15);
+
+            expect(warmDestinations.length).toBe(2);
+            expect(warmDestinations[0].city).toBe("Beijing");
+            expect(warmDestinations[1].city).toBe("Lima");
+
+        }));
+    });
+
+    describe('Testing AngularJS Directive', function(){
+
+        var scope, template, httpBackend, isolateScope, rootScope;
+
+        // Mock the helper service using $provide, so we'll use this function instead of the one on the service
+        beforeEach(function(){
+            module(function($provide){
+                var MockedHelperService = {
+                    convertKelvinToCelsius: function(temp){
+                        return Math.round(temp - 273);
+                    }
+                };
+
+                $provide.value('helperService', MockedHelperService); 
+            });
+        });
+
+        beforeEach(inject(function($compile, $rootScope, $httpBackend, _helperService_){
+            scope = $rootScope.$new();
+            httpBackend = $httpBackend;
+            rootScope = $rootScope;
+            helperService = _helperService_;
+
+            scope.destination = {
+                city: "Tokyo",
+                country: "Japan"
+            };
+
+            scope.apiKey = "xyz";
+
+            var element = angular.element(
+                '<div destination-directive destination="destination" api-key="apiKey" on-remove="remove()"></div>'
+            );
+
+            // Compile the template into JS
+            template = $compile(element)(scope);
+            scope.$digest();
+
+            // Get the directive isolate scope to access the methods
+            isolateScope = element.isolateScope();
+        }));
+
+        it('should update the weather for the specific destination', function() {
+
+            // This tells karma and jasmine to just execute the function as it normally would by delegating to it actual code
+            // spyOn(helperService, 'convertKelvinToCelsius').and.callThrough();
+
+            // Mocks the return value
+            // spyOn(helperService, 'convertKelvinToCelsius').and.returnValue(15);
+
+            // Mocks the entire function in a more granular way (can use spies or $provide to mock any dependencies)
+            spyOn(helperService, 'convertKelvinToCelsius').and.callFake(function(temp){
+                return temp - 273;
+            });
+
+            scope.destination = {
+                city: "Cartago",
+                country: "Costa Rica"
+            };
+
+            // Mock the http call to simulate the request and attach a response
+            httpBackend.expectGET("http://api.openweathermap.org/data/2.5/weather?q="+scope.destination.city+"&appid="+scope.apiKey).respond(
+                {
+                    weather: [{main: 'Rain', detail: 'Light rain'}],
+                    main: {temp: 288}
+                });
+
+            // Access getWeather method throught isolateScope
+            isolateScope.getWeather(scope.destination);
+
+            // End any http request
+            httpBackend.flush();
+
+            expect(scope.destination.weather.main).toBe("Rain");
+            expect(scope.destination.weather.temp).toBe(15);
+            expect(helperService.convertKelvinToCelsius).toHaveBeenCalledWith(288);
+
+        });
+
+        it('should add a message if no city is found', function() {
+            scope.destination = {
+                city: "Cartago",
+                country: "Costa Rica"
+            };
+
+            // Mock the http call to simulate the request and attach a response
+            httpBackend.expectGET("http://api.openweathermap.org/data/2.5/weather?q="+scope.destination.city+"&appid="+scope.apiKey).respond(
+                { }
+            );
+
+            // Access getWeather method throught isolateScope
+            isolateScope.getWeather(scope.destination);
+
+            // End any http request
+            httpBackend.flush();
+
+            expect(rootScope.message).toBe("City not found");
+
+        });
+
+        it('should add a message when there is a server error', function() {
+
+            // Checks if a function have been called
+            spyOn(rootScope, '$broadcast');
+            
+            scope.destination = {
+                city: "Cartago",
+                country: "Costa Rica"
+            };
+
+            // Mock the http call to simulate the request and attach a response
+            httpBackend.expectGET("http://api.openweathermap.org/data/2.5/weather?q="+scope.destination.city+"&appid="+scope.apiKey).respond(500);
+
+            // Access getWeather method throught isolateScope
+            isolateScope.getWeather(scope.destination);
+
+            // End any http request
+            httpBackend.flush();
+
+            expect(rootScope.message).toBe("Server Error");
+            
+            // Check if the spy captured a function call (in the parameter just include the function name)
+            expect(rootScope.$broadcast).toHaveBeenCalled();
+            expect(rootScope.$broadcast).toHaveBeenCalledWith('messageUpdated', { type: 'error', message: 'Server error' }); 
+            expect(rootScope.$broadcast.calls.count()).toBe(1);
+
+        });
+
+        it('should call the parent controller remove function', function(){
+            scope.removeTest = 1;
+
+            // Mock the remove function
+            scope.remove = function(){
+                scope.removeTest++;
+            };
+
+            isolateScope.onRemove();
+
+            expect(scope.removeTest).toBe(2);
+        }); 
+ 
+        it('should generate the correct HTML', function(){
+            var templateAsHtml = template.html();
+
+            expect(templateAsHtml).toContain('Tokyo, Japan');
+
+            scope.destination.city = "London";
+            scope.destination.country = "England";
+
+            // Run the digest cycle and generate the template again
+            scope.$digest();
+            templateAsHtml = template.html();
+
+            expect(templateAsHtml).toContain('London, England');
 
         });
 
